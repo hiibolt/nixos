4d507a3e33429bee3a2f039a4f7e3531fe3041ed
BoltR6 <johnwallacewhite@gmail.com>
2023-05-29 16:25:56 -0500

    Added advanced boilerplate command

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,3 +6,25 @@ end
 function t
     eval "$TEST_COMMAND"
 end
+function cfg
+    cd /etc/nixos
+    xdg-open .
+    code .
+end
+function boilerplate -d "Grabs a boilerplate from https://github.com/boltr6/nix-templates"
+    if contains -- "$argv" "-L" "--list"
+        # List the available boilerplates
+        git clone -q https://github.com/boltr6/nix-templates
+        echo "Availabe boilerplates:"
+        ls "$PWD/nix-templates"
+        rm -R -f nix-templates
+    else
+        # Clone and move the selected boilerplate
+        git clone -q https://github.com/boltr6/nix-templates
+        echo "Grabbing the following files:"
+        ls "$PWD/nix-templates/$argv[1]"
+        mv -n "$PWD/nix-templates/$argv[1]/"* "$PWD"
+        rm -R -f nix-templates
+        echo "Done"
+    end
+end
