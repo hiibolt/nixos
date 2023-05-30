a07d4e35d5664f1e6be0e29c3143d60d18c913e7
BoltR6 <johnwallacewhite@gmail.com>
2023-05-29 23:45:41 -0500

    Improved CBT commands

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,32 @@
 set -x DIRENV_LOG_FORMAT ""
 eval (direnv hook fish)
+function c
+    set_color -o red
+    echo "Cleanup phase '$CLEAN_COMMAND'"
+    set_color normal
+    eval "$CLEAN_COMMAND"
+end
 function b
+    set_color -o yellow
+    echo "Build phase '$BUILD_COMMAND'"
+    set_color normal
     eval "$BUILD_COMMAND"
 end
 function t
+    set_color -o green
+    echo "Test phase '$TEST_COMMAND'"
+    set_color normal
     eval "$TEST_COMMAND"
+end
+function cbt
+    c
+    b
+    t
+end
+function e
+    code . &
+    disown
+    exit
 end
 function cfg
     cd /etc/nixos
@@ -22,8 +44,8 @@ function boilerplate -d "Grabs a boilerplate from https://github.com/boltr6/nix-
         # Clone and move the selected boilerplate
         git clone -q https://github.com/boltr6/nix-templates
         echo "Grabbing the following files:"
-        ls "$PWD/nix-templates/$argv[1]"
-        mv -n "$PWD/nix-templates/$argv[1]/"* "$PWD"
+        ls -A "$PWD/nix-templates/$argv[1]"
+        mv -n "$PWD/nix-templates/$argv[1]/"{.*,*} "$PWD"
         rm -R -f nix-templates
         echo "Done"
     end
