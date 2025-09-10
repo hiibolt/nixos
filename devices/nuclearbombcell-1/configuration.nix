475f03ccec73ad2daea962563fa2a511fc3c568a
hiibolt <me@hiibolt.com>
2025-09-09 19:21:45 -0500

    Revert "cleanup: Removed old configs"
    This reverts commit 8bddd442127838947edc1a42801b27fc84f0f0a5.

---
+++ devices/nuclearbombcell-1/configuration.nix
@@ -0,0 +1,72 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{ config, lib, pkgs, inputs, ... }:
+let
+  this_device_dir = ./.;
+  lib_dir         = ../../lib;
+  users_dir       = ../../users;
+  workloads_dir   = ../../workloads;
+  hardware_dir    = ../../hardware;
+
+  system = {
+    cpu = "intel";
+    gpu = "amd";
+    background = "6.jpg";
+
+    keyboard = {
+    layout = "semimak";
+    device_id = "usb-SteelSeries_Apex_Pro_TKL_Wireless-if01-event-kbd";
+    };
+  };
+in
+{
+  imports =
+    [
+      # Hardware
+      (import ../../lib/disko/default.nix { device = "/dev/sda"; })
+      "${this_device_dir}/hardware-configuration.nix"
+      "${hardware_dir}/cpus/${system.cpu}.nix"
+
+      # System Shell
+      "${lib_dir}/shell/default.nix"
+
+      # System drivers and daemons
+      "${lib_dir}/maintenance/default.nix"
+      "${lib_dir}/common/default.nix"
+      "${lib_dir}/impermanence/default.nix"
+
+      # Users
+      "${users_dir}/hiibolt/user.nix"
+      "${users_dir}/root/default.nix"
+      "${users_dir}/groups.nix"
+    ];
+
+  # Disable Auto-Suspend
+  systemd.targets = {
+    sleep.enable = false;
+    suspend.enable = false;
+    hibernate.enable = false;
+    hybrid-sleep.enable = false;
+  };
+
+  # Networking
+  networking = {
+    hostName = "nuclearbombcell-1";
+    networkmanager.enable = true;
+  };
+
+  # System Packages
+	environment.systemPackages = with pkgs; [
+    # Basic Development
+		vim
+		wget
+  ];
+  
+  # State Version - Change with caution,
+  #  but it's worth noting that since this
+  #  system uses impermanent storage, it's
+  #  likely a lot easier to recover from
+  system.stateVersion = "24.11";
+}
---
+++ devices/nuclearbombsoc/configuration.nix
@@ -0,0 +1,101 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{ config, lib, pkgs, inputs, system, ... }:
+let
+  this_device_dir = ./.;
+  lib_dir         = ../../lib;
+  users_dir       = ../../users;
+  workloads_dir   = ../../workloads;
+  hardware_dir    = ../../hardware;
+
+  system = {
+    cpu = "intel";
+    gpu = "nvidia";
+    background = "6.jpg";
+
+    keyboard = {
+    layout = "semimak";
+    device = "by-path/platform-i8042-serio-0-event-kbd";
+    };
+  };
+in
+{
+  imports =
+    [
+      # Hardware
+      (import ../../lib/disko/default.nix { device = "/dev/nvme0n1"; })
+      "${this_device_dir}/hardware-configuration.nix"
+      "${hardware_dir}/cpus/${system.cpu}.nix"
+      "${hardware_dir}/gpus/${system.gpu}.nix"
+
+      # System Shell
+      "${lib_dir}/shell/default.nix"
+
+      # System drivers and daemons
+      (import "${lib_dir}/kanata/default.nix" {
+        inherit config;
+        inherit pkgs;
+        layout = system.keyboard.layout;
+        device = system.keyboard.device;
+      })
+      "${lib_dir}/impermanence/default.nix"
+      "${lib_dir}/maintenance/default.nix"
+      "${lib_dir}/common/default.nix"
+      "${lib_dir}/fingerprint/default.nix"
+
+      # Workloads
+      "${workloads_dir}/deployments/mc"
+
+      # Users
+      "${users_dir}/hiibolt/user.nix"
+      "${users_dir}/root/default.nix"
+      "${users_dir}/groups.nix"
+    ];
+
+  # Enable the X11 windowing system with KDE Plasma 6
+  services.xserver.enable = true;
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
+  # Stylix
+	stylix = {
+    enable = true;
+    image = /etc/nixos/backgrounds/6.jpg;
+	};
+  
+
+	# Allow unfree packages and enable Nix Flakes
+	nixpkgs.config.allowUnfree = true;
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Networking
+  networking = {
+    hostName = "nuclearbombsoc";
+    networkmanager.enable = true;
+  };
+  services.openssh.enable = true;
+  services.tailscale = {
+    enable = true;
+    extraSetFlags = [
+      "--ssh"
+    ];
+  };
+
+  # System Packages
+	environment.systemPackages = with pkgs; [
+    # Basic Development
+		vim
+		wget
+
+    # Web Browsing
+		librewolf
+  ];
+  
+  # State Version - Change with caution,
+  #  but it's worth noting that since this
+  #  system uses impermanent storage, it's
+  #  likely a lot easier to recover from
+  system.stateVersion = "24.11";
+}
